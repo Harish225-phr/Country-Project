5cafd990056fb564ebabbb489f7ca848e218f706
GMB Developer <test@example.com>
2026-05-15 14:39:43 +0530

    Refactor country details display; fetch country data dynamically and update UI elements

--- card.js
+++ card.js
@@ -1,26 +1,42 @@
 const params = new URLSearchParams(window.location.search)
 
 const countryNam = params.get('name')
-const countryImg = params.get('img')
-const countryPop = params.get('population')
-const countryReg = params.get('region')
-const countryCap = params.get('capital')
-const imgDesc = params.get('dec')
-
-let cardImage = document.querySelector('.card-image')
-cardImage.src = countryImg;
-
-let countryName = document.querySelector('.country-name');
-countryName.innerText = countryNam;
-
-let countryPopulation = document.querySelector('.country-population');
-countryPopulation.innerText = countryPop;
-
-let countryRegion = document.querySelector('.country-region');
-countryRegion.innerText = countryReg;
-
-let countryCapital = document.querySelector('.country-capital');
-countryCapital.innerText = countryCap;
-
 let countryDec = document.querySelector('.country-desc');
-countryDec.innerText = imgDesc;
+let countryName = document.querySelector('.country-name');
+let cardImage = document.querySelector('.card-image')
+let countryPopulation = document.querySelector('.country-population');
+let countryRegion = document.querySelector('.country-region');
+let countryCapital = document.querySelector('.country-capital');
+let borderCountry = document.querySelector('.border-country');
+let borderCountryContainer = document.querySelector('.brder-country-container');
+
+
+
+fetch(`https://restcountries.com/v3.1/name/${countryNam}?fullText=true`)
+.then((res) => res.json())
+.then((data) => {
+    console.log(data[0])
+    countryDec.innerText = data[0].flags.alt
+    cardImage.src = data[0].flags.png
+    countryName.innerText = countryNam
+    countryPopulation.innerText = data[0].population
+    countryRegion.innerText = data[0].region
+    countryCapital.innerText = data[0].capital
+    data[0].borders.forEach((borer) => {
+    let createButton = document.createElement('button');
+    createButton.classList.add(
+  'btn',
+  'btn-light',
+  'shadow-sm',
+  'btn-sm',
+  'px-3',
+  'border-country'
+)
+    createButton.innerText = borer
+    borderCountryContainer.appendChild(createButton)
+    })
+})
+
+
+
+
